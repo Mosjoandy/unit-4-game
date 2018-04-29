8b4965436547aaab78697d4da01785d5774940f8
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2018-04-28 19:35:15 -0700

    random gem numbers will add up now!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,19 +8,29 @@ var gem2rando = Math.floor(Math.random() * 13) + 1;
 var gem3rando = Math.floor(Math.random() * 13) + 1;
 var gem4rando = Math.floor(Math.random() * 13) + 1;
 
-$("#gem1").on("click", function() {
-    console.log("You clicked a gem: " + gem1rando);
-})
+// Gemstones are hooked to on.click function
+    $("#gem1").on("click", function() {
+        console.log("You clicked a gem: " + gem1rando);
+    //adding clicked gem number to "attempt"
+        attempt += gem1rando;
+        console.log("Your total attempt: " + attempt);
+    })
 
-$("#gem2").on("click", function() {
-    console.log("You clicked a gem: " + gem2rando);
-})
+    $("#gem2").on("click", function() {
+        console.log("You clicked a gem: " + gem2rando);
+        attempt += gem2rando;
+        console.log("Your total attempt: " + attempt);
+    })
 
-$("#gem3").on("click", function() {
-    console.log("You clicked a gem: " + gem3rando);
-})
+    $("#gem3").on("click", function() {
+        console.log("You clicked a gem: " + gem3rando);
+        attempt += gem3rando;
+        console.log("Your total attempt: " + attempt);
+    })
 
-$("#gem4").on("click", function() {
-    console.log("You clicked a gem: " + gem4rando);
-})
+    $("#gem4").on("click", function() {
+        console.log("You clicked a gem: " + gem4rando);
+        attempt += gem4rando;
+        console.log("Your total attempt: " + attempt);
+    })
 
